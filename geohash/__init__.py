81a02c895d9f9122c0ce755264bf22eff1603449
Gustavo Maia <gurumaia@gmail.com>
2019-03-29 20:13:33 -0300

    Refactor back to simple project structure

--- geohash/__init__.py
+++
@@ -1,1 +0,0 @@
-from .geohash import handler
--- src/test_handler.py
+++ src/test_handler.py
@@ -1,24 +1,24 @@
 import pytest
-from geohash import handler, geohash
+import handler
 
 
 def test_geohash_success():
-    assert geohash.geohash(37.421542, -122.085589, b'2005-05-26-10458.68') == {'latitude': 37.857713, 'longitude': -122.544543}
+    assert handler.geohash(37.421542, -122.085589, b'2005-05-26-10458.68') == {'latitude': 37.857713, 'longitude': -122.544543}
 
 
 def test_geohash_string():
     with pytest.raises(TypeError):
-        assert geohash.geohash(37.421542, -122.085589, '2005-05-26-10458.68') is None
+        assert handler.geohash(37.421542, -122.085589, '2005-05-26-10458.68') is None
 
 
 def test_geohash_no_coord():
     with pytest.raises(TypeError):
-        assert geohash.geohash(None, None, b'2005-05-26-10458.68') is None
+        assert handler.geohash(None, None, b'2005-05-26-10458.68') is None
 
 
 def test_geohash_no_date():
     with pytest.raises(TypeError):
-        assert geohash.geohash(37.421542, -122.085589, None) is None
+        assert handler.geohash(37.421542, -122.085589, None) is None
 
 
 def test_handler_success():
@@ -31,4 +31,4 @@ def test_handler_success():
     }
 
     r = {'statusCode': 200, 'body': '{"coordinates": {"latitude": 37.857713, "longitude": -122.544543}, "url": "https://www.google.com/maps/place/37.857713,-122.544543"}'}
-    assert geohash.handler(i, '') == r
+    assert handler.handler(i, '') == r
